bbf48fbc45576f4006bacb07e1f7048473439cb5
Mohanen <bmohanen@gmail.com>
2019-12-19 01:33:26 +0530

    added md file show feature with showdown

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-// Our data object
-var data = { githubFolders: [] };
 const repoDetails = {
     github_api_url: "https://api.github.com/repos",
     username: "mohanen",
@@ -8,34 +6,73 @@ const repoDetails = {
 const repo_url = repoDetails.github_api_url + "/" + repoDetails.username + "/" + repoDetails.repo_name;
 const repo_content_url = repo_url + "/contents";
 
+var defaultOptions = showdown.getDefaultOptions();
+defaultOptions.simplifiedAutoLink = true;
+defaultOptions.tables = true;
+defaultOptions.smartIndentationFix = true;
+defaultOptions.requireSpaceBeforeHeadingText = true;
+defaultOptions.openLinksInNewWindow = true;
+defaultOptions.emoji = true;
+var converter = new showdown.Converter(defaultOptions);
+converter.setFlavor('github');
 
-var vm = new Vue({
+var vueFolder = new Vue({
     el: '#folders',
-    data: data
-    // options
+    data: { githubFolders: [], enabled: true }
 })
 
-axios
-    .get(repo_content_url)
-    .then(response => {
-        data.githubFolders = response.data;
-        data.githubFolders.forEach((folder, index) => {
-            axios
-                .get(folder.url)
-                .then(response => {
-                    data.githubFolders[index].files = response.data;
-                    vm.$forceUpdate();
-                })
-        });
-    })
+var vueFile = new Vue({
+    el: '#file',
+    data: { md: "", enabled: false }
+})
+
+function updateHome() {
+    axios
+        .get(repo_content_url)
+        .then(response => {
+            vueFolder.githubFolders = response.data;
+            vueFolder.githubFolders.forEach((folder, index) => {
+                axios
+                    .get(folder.url)
+                    .then(response => {
+                        vueFolder.githubFolders[index].files = response.data;
+                        vueFolder.$forceUpdate();
+                    })
+            });
+        })
+}
+
+function openMd(mdFileUrl) {
+    vueFolder.enabled = false;
+    vueFile.enabled = true;
+
+    axios
+        .get(mdFileUrl)
+        .then(response => {
+            vueFile.md = response.data;
+            console.log(response.data);
+            vueFile.$forceUpdate();
+        })
+}
+
+function home() {
+    vueFolder.enabled = true;
+    vueFile.enabled = false;
+    updateHome();
+}
+
 
 // helper functions
 const hf = {
-    toSentenceCase : function (text) {
-        var result = text.replace( /([A-Z])/g, " $1" );
+    toSentenceCase: function (text) {
+        var result = text.replace(/([A-Z])/g, " $1");
         return result.charAt(0).toUpperCase() + result.slice(1);
     },
     toHeaderCase: text => (hf.toSentenceCase(text).toUpperCase()),
     toMdFileCase: text => (hf.toSentenceCase(text).slice(0, -".md".length)),
-
+    md2Html: function (mdString) {
+        return converter.makeHtml(mdString);
+    }
 }
+
+home();
