fab9a2a8344c1aebdee0ace3a0edd7849f18aa3b
Mohanen <bmohanen@gmail.com>
2019-12-20 00:20:33 +0530

    adding classes

--- cheatSheetParser.js
+++ cheatSheetParser.js
@@ -3,14 +3,21 @@ function cheatSheetParser(HtmlString) {
 
     HPrntDivClass = {
         1: "before section",
-        2: "before",
-        3: "before",
+        2: "before column card",
+        3: "before box",
         4: "before",
     }
     HPrntDivClassInner = {
         1: "after columns",
-        2: "after column",
-        3: "after box",
+        2: "after card-content",
+        3: "after ",
+        4: "after",
+    }
+
+    headerClass = {
+        1: "after",
+        2: "is-primary is-light card-header card-header-title",
+        3: "after",
         4: "after",
     }
 
@@ -29,19 +36,20 @@ function cheatSheetParser(HtmlString) {
                     i += element_div.length;
                 }
             }
-            headersStack.push(parseInt(HtmlString[i]))
-            element_div = "<div class='" + HPrntDivClass[parseInt(HtmlString[i])] + "'>"
-            header_class = " class='column-head' "
-            HtmlString = HtmlString.slice(0, i - 2) + element_div + HtmlString.slice(i - 2, i + 1) + header_class + HtmlString.slice(i + 1)
-            i += element_div.length + header_class.length;
+            headerWeight = parseInt(HtmlString[i]);
+            headersStack.push(headerWeight)
+            element_div = "<div class='" + HPrntDivClass[headerWeight] + "'>"
+            headerClassTag = " class='" + headerClass[headerWeight] + "' "
+            HtmlString = HtmlString.slice(0, i - 2) + element_div + HtmlString.slice(i - 2, i + 1) + headerClassTag + HtmlString.slice(i + 1)
+            i += element_div.length + headerClassTag.length;
 
         } else if (HtmlString[i] == '/' && HtmlString[i + 1] == 'h') {
             i += 2;
-            header_weight = parseInt(HtmlString[i]);
-            headersStack.push(header_weight)
+            headerWeight = parseInt(HtmlString[i]);
+            headersStack.push(headerWeight)
             while (HtmlString[i++] != '>');
             i++;
-            element_div = "<div class='" + HPrntDivClassInner[header_weight] + "'>"
+            element_div = "<div class='" + HPrntDivClassInner[headerWeight] + "'>"
             HtmlString = HtmlString.slice(0, i) + element_div + HtmlString.slice(i)
             i += element_div.length;
 
