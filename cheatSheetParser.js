f1d91051dfbc4c5347bb3e6267fb0892e1da363a
Mohanen <bmohanen@gmail.com>
2019-12-21 20:27:39 +0530

    style changes

--- cheatSheetParser.js
+++ cheatSheetParser.js
@@ -1,7 +1,7 @@
 function cheatSheetParser(HtmlString) {
     HeaderWrapperAttr = {
         1: 'class=""',
-        2: 'class="col-4 mw-400"',
+        2: 'class="col-4 mw-300"',
         3: 'class="cheat-card-content ovrflw-auto"',
     }
     HeaderChildWrapperAttr = {
